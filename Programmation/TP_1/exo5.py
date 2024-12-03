929c3d14c07ff2ec5ca02b00db2ab8ea454e0148
STILISKY <stilisky@HP-EliteBook-840-G3-913e967f.mshome.net>
2024-12-03 19:01:44 +0100

    Exo 5 added

--- Programmation/TP_1/exo5.py
+++ Programmation/TP_1/exo5.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
 
+#Redefine key
+def get_key(key):
+    if(new_key<=0 and new_key>=26):
+        return new_key
+    else:
+        val = key % 26
+        return val
+
 #abcdefgmhijklmnopqrstuvwxyz
 alpha_principal = "abcdefghijklmnopqrstuvwxyz"
 crypte_alpha = ''
 message = input("Entrer le message: ")
 msg_crypt = ''
-clef = int(input("Entrer la clef: "))
+new_key = int(input("Entrer la clef: "))
+#Get new key
+clef = get_key(new_key)
+
 for letter in alpha_principal:
     position = alpha_principal.find(letter)
     if((position + clef + 1)  < 27):
-        crypte_alpha +=  alpha_principal[position:(position +1)]
+        crypte_alpha +=  alpha_principal[(position + clef):(position + clef +1)]
     else :
-        sous = 26 - (position + clef + 1) 
+        val = position + clef +1
+        sous = val - 26 
         crypte_alpha += alpha_principal[sous: (sous+1)]
+    # print("letter: ", letter, " posi: ", position, "Crypte: ",crypte_alpha)
 
 for val in message:
     pos = alpha_principal.find(val)
@@ -21,7 +34,8 @@ for val in message:
     else:
         msg_crypt += val
 
-print(alpha_principal)
-print(crypte_alpha)
+# print()
+# print(alpha_principal)
+# print(crypte_alpha)
 print ("Message chiffre: ", msg_crypt)
 
