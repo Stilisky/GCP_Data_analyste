5044676d69f354533ac146e970436ec4c9ec81de
Stilisky <setonde.hounnouvi@epitech.eu>
2024-12-04 20:41:24 +0100

    Exo3 files added

---
+++ Programmation/TP_3/exo2/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import multip
+
+multip.multiFunc(8)
---
+++ Programmation/TP_3/exo2/multip.py
@@ -0,0 +1,6 @@
+
+def multiFunc(number):
+    for i in range(13):
+        result = i*number
+        print(number, " X ", i, " = ", result)
+
