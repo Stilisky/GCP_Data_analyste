26669c31669fe3a76682402a292a27d4f99ae8cf
Stilisky <setonde.hounnouvi@epitech.eu>
2024-12-04 20:36:06 +0100

    Exo1 files added

---
+++ Programmation/TP_3/exo1/display.py
@@ -0,0 +1,7 @@
+
+def hello():
+    print("Bonjour SEME-CITY 2024")
+
+def hello_n(n):
+    for _ in range(n):
+        hello()
---
+++ Programmation/TP_3/exo1/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import display
+
+display.hello_n(5)
